1cd1f1bd9148745dd7cc909897628c03c56ca2d4
Mozart Muhammad M <mozart.muhammad@gmail.com>
2013-11-16 18:55:43 +0700

    res + src

---
+++ src/sport/app/SportsPicker.java
@@ -0,0 +1,22 @@
+package sport.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SportsPicker extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sports_picker);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sports_picker, menu);
+		return true;
+	}
+
+}
