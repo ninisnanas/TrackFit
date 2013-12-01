a4230ef06f9c7f5754892f9923297b5c7f3fb0dc
Mozart Muhammad M <mozart.muhammad@gmail.com>
2013-12-01 21:31:32 +0700

    sporttrack

--- src/project/trackfit/view/MenuSportTrack.java
+++ src/project/trackfit/view/MenuSportTrack.java
@@ -44,13 +44,18 @@ public class MenuSportTrack extends Activity implements LocationListener,
 	boolean isStart;
 	float avgSpeed;
 	int point;
+	float jarakSampeSekarang;
+	float jarakAwalAkhir;
+	int minutes;
+
 	float distance;
+	float distanceTo;
 
 	int invisible = View.GONE;
 	int visible = View.VISIBLE;
 
 	private TextView TVstopwatch;
-	private TextView TVDistance;
+
 	private TextView TVAvgSpeed;
 	private Button home;
 	private Button history;
@@ -62,11 +67,11 @@ public class MenuSportTrack extends Activity implements LocationListener,
 	private Button resume;
 	private Button stop;
 
+	Location lokasLama;
 	Location initialLocation;
 	Location lastLocation;
 	Location finalLocation;
-	float distanceTo;
-	
+
 	private long timeInMilliseconds = 0L;
 	private long startTime = 0L;
 	private long updatedTime = 0L;
@@ -78,8 +83,7 @@ public class MenuSportTrack extends Activity implements LocationListener,
 
 	Handler timerHandler = new Handler();
 	int counter = 0;
-	
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -88,10 +92,11 @@ public class MenuSportTrack extends Activity implements LocationListener,
 		routePoints = new ArrayList<LatLng>();
 		setupView();
 		setupEvent();
+		minutes = 0;
 
 		try {
 			Log.d("test", "masuk try cacth");
-			
+
 			initializeMap();
 			isStart = false;
 		} catch (Exception e) {
@@ -102,7 +107,7 @@ public class MenuSportTrack extends Activity implements LocationListener,
 
 	private void setupView() {
 		TVstopwatch = (TextView) findViewById(R.id.textViewDuration);
-		TVDistance = (TextView) findViewById(R.id.textViewDistance);
+
 		TVAvgSpeed = (TextView) findViewById(R.id.textViewAvgSpeed);
 		home = (Button) findViewById(R.id.HomeIconButton);
 		history = (Button) findViewById(R.id.HistoryIconButton);
@@ -159,18 +164,16 @@ public class MenuSportTrack extends Activity implements LocationListener,
 					.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
 			finish();
 		} else if (v.equals(start)) {
-			if(prepareToRun()){
-				//ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-				//NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-				//if (networkInfo != null && networkInfo.isConnected() && isGPSEnabled() && isNetworkEnabled()){
-					isStart=true;
-					start.setVisibility(invisible);
-					pause.setVisibility(visible);
-					resume.setVisibility(invisible);
-					stop.setVisibility(visible);
-					startTime = SystemClock.uptimeMillis();
-					timerHandler.postDelayed(timerRunnable, 0);
-				//}
+			if (readyToRun()) {
+
+				isStart = true;
+				start.setVisibility(invisible);
+				pause.setVisibility(visible);
+				resume.setVisibility(invisible);
+				stop.setVisibility(visible);
+				startTime = SystemClock.uptimeMillis();
+				timerHandler.postDelayed(timerRunnable, 0);
+
 			}
 		} else if (v.equals(pause)) {
 			isStart = false;
@@ -198,22 +201,32 @@ public class MenuSportTrack extends Activity implements LocationListener,
 			timerHandler.removeCallbacks(timerRunnable);
 			stopTrain();
 			trainingSummary();
+			setVaribleToDefault();
 
-			//Toast.makeText(getApplicationContext(), "" + updatedTime + "haha",
-			//		Toast.LENGTH_LONG).show();
+			// Toast.makeText(getApplicationContext(), "" + updatedTime +
+			// "haha",
+			// Toast.LENGTH_LONG).show();
 		}
 	}
 
-	// calculate distance between 2 point
-	private float calculateDistance(LatLng startPoint, LatLng endPoint) {
-		
-		float[] result = new float[1];
-		Location.distanceBetween(startPoint.latitude, startPoint.longitude,
-				endPoint.latitude, endPoint.longitude, result);
-		return result[0];
+	private void setVaribleToDefault() {
+		avgSpeed = 0f;
+		point = 0;
+		jarakSampeSekarang = 0f;
+		jarakAwalAkhir = 0f;
+		minutes = 0;
+		distance = 0f;
+		distanceTo = 0f;
+		lokasLama = null;
+		initialLocation = null;
+		lastLocation = null;
+		finalLocation = null;
+		totalDistance = 0f;
+		startPos = null;
+		endPos = null;
+		counter = 0;
+
 	}
-	
-	
 
 	private float calculateDistance(Location startLoc, Location endLoc) {
 		distanceTo = endLoc.distanceTo(startLoc);
@@ -224,11 +237,26 @@ public class MenuSportTrack extends Activity implements LocationListener,
 		return result[0];
 	}
 
+	private float calculateAvgSpeed(Location loc) {
+		float s = loc.getSpeed();
+		float avg = ((avgSpeed * (point - 1)) + s) / point;
+		return avg;
+	}
+
+	private double calculateCalories(int age, int weight, float timeMinute) {
+		int heartRate = 120;
+		double c = ((age * 0.2017) + (weight * 0.09036) + (heartRate * 0.6309) - 55.0969)
+				* timeMinute / 4.184;
+		// [(Age x 0.2017) + (Weight x 0.09036) + (Heart Rate x 0.6309) --
+		// 55.0969] x Time / 4.184.
+		return c;
+	}
+
 	// enable features of the overlay
 	@Override
 	protected void onResume() {
 		super.onResume();
-		//initializeMap();
+		// initializeMap();
 	}
 
 	// disable features of the overlay when in the background
@@ -249,27 +277,13 @@ public class MenuSportTrack extends Activity implements LocationListener,
 			secs = secs % 60;
 			int milliSecs = (int) (updatedTime % 1000);
 			TVstopwatch.setText("" + hours + ":" + String.format("%02d", mins)
-					+ ":" + String.format("%02d", secs) + ":"
-					+ String.format("%02d", milliSecs));
+					+ ":" + String.format("%02d", secs));
+
+			minutes = mins;
+
 			timerHandler.postDelayed(this, 0);
 
-			// draw tracking lines
-			/*
-			 * if (secs % 3 == 0) { LatLng currentPoint = new
-			 * LatLng(map.getMyLocation().getLatitude(),
-			 * map.getMyLocation().getLongitude());
-			 * 
-			 * 
-			 * 
-			 * }
-			 */
-			//if (secs % 10 == 0) {
-			//	LatLng currentLocation = new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude());
-			//	routePoints.add(currentLocation);
-	
-			//	Polyline route = map.addPolyline(new PolylineOptions().geodesic(true));
-			//	route.setPoints(routePoints);
-			//}
+			
 		}
 	};
 
@@ -280,35 +294,40 @@ public class MenuSportTrack extends Activity implements LocationListener,
 					.findFragmentById(R.id.map)).getMap();
 
 			map.setMyLocationEnabled(true);
-		
+
 			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 			Criteria criteria = new Criteria();
 			Location currentLocation = locationManager
 					.getLastKnownLocation(locationManager.getBestProvider(
 							criteria, false));
-			
-			//Log.d("test", "haha");
-			//Log.d("test", currentLocation.getLatitude()+":"+currentLocation.getLongitude());
-			
-			CameraPosition camPos = new CameraPosition.Builder().target(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude())).zoom(15.8f).build();
-			CameraUpdate camUpdate = CameraUpdateFactory.newCameraPosition(camPos);
-			
+
+			// Log.d("test",
+			// "initial map "+currentLocation.getLatitude()+":"+currentLocation.getLongitude());
+
+			CameraPosition camPos = new CameraPosition.Builder()
+					.target(new LatLng(currentLocation.getLatitude(),
+							currentLocation.getLongitude())).zoom(15.8f)
+					.build();
+			CameraUpdate camUpdate = CameraUpdateFactory
+					.newCameraPosition(camPos);
+
 			map.moveCamera(camUpdate);
 
-			MarkerOptions marker = new MarkerOptions()
-					.position(
-							new LatLng(currentLocation.getLatitude(),
-									currentLocation.getLongitude()))
-					.title("You're here");
+			MarkerOptions marker = new MarkerOptions().position(
+					new LatLng(currentLocation.getLatitude(), currentLocation
+							.getLongitude())).title("You're here");
 
 			map.addMarker(marker);
-			
-			//startPos = new Location(currentLocation);
-			//Log.d("test", "start="+startPos.getLatitude()+":"+startPos.getLongitude());
-			
+
+			// startPos = new Location(currentLocation);
+			// Log.d("test",
+			// "start="+startPos.getLatitude()+":"+startPos.getLongitude());
+
 			initialLocation = currentLocation;
 			lastLocation = currentLocation;
 			finalLocation = currentLocation;
+			Toast.makeText(getApplicationContext(), "InitialMap",
+					Toast.LENGTH_SHORT).show();
 			if (map == null) {
 				Toast.makeText(getApplicationContext(),
 						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
@@ -317,66 +336,64 @@ public class MenuSportTrack extends Activity implements LocationListener,
 		}
 
 	}
-	private boolean prepareToRun() {
 
-		
+	private boolean readyToRun() {
+
 		boolean isNetworkOK = isNetworkEnabled();
 		boolean isGPSOK = isGPSEnabled();
+		jarakAwalAkhir = 0f;
+		jarakSampeSekarang = 0f;
 		avgSpeed = 0f;
 		point = 0;
-		
+
 		ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-		
+
 		if (networkInfo != null && networkInfo.isConnected()) {
-		
-			if(isNetworkOK && isGPSOK) {
-				
-				
-				if(isGPSOK) {
-					locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+
+			if (isNetworkOK && isGPSOK) {
+				if (isNetworkOK) {
+					locationManager.requestLocationUpdates(
+							LocationManager.NETWORK_PROVIDER, 1000, 1, this);
 				}
-				if(isNetworkOK) {
-					locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, this);
+
+				if (isGPSOK) {
+					locationManager.requestLocationUpdates(
+							LocationManager.GPS_PROVIDER, 1000, 1, this);
 				}
+
 				isStart = true;
-				//makeMarker(initialLocation, "START");
+				// makeMarker(initialLocation, "START");
 				return true;
 			} else {
 				return false;
 			}
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	private void stopTrain() {
-		//Criteria crit = new Criteria();
-		//finalLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(crit, false));
-		//totalDistance = calculateDistance(initialLocation, finalLocation);
+		// Criteria crit = new Criteria();
+		// finalLocation =
+		// locationManager.getLastKnownLocation(locationManager.getBestProvider(crit,
+		// false));
+		// totalDistance = calculateDistance(initialLocation, finalLocation);
 		Criteria crit = new Criteria();
-		finalLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(crit, false));
-		if(initialLocation != null && finalLocation != null) {
+		finalLocation = locationManager.getLastKnownLocation(locationManager
+				.getBestProvider(crit, false));
+		if (initialLocation != null && finalLocation != null) {
 			totalDistance = calculateDistance(initialLocation, finalLocation);
-			//makeMarker(finalLocation, "FINISH");
+			// makeMarker(finalLocation, "FINISH");
 		}
 	}
 
-	private float calculateAvgSpeed(Location loc) {
-
-		float s = loc.getSpeed();
-
-		float avg = ((avgSpeed * (point - 1)) + s) / point;
-		return avg;
-	}
-	
-
-	
-	
 	private void trainingSummary() {
-		Toast.makeText(getApplicationContext(),"speed: " + avgSpeed + " distance: " + totalDistance +" Time: "+ updatedTime, Toast.LENGTH_LONG)
-				.show();
+		jarakAwalAkhir = jarakSampeSekarang;
+		Toast.makeText(
+				getApplicationContext(),
+				"speed: " + avgSpeed + " distance: " + totalDistance
+						+ "calories boong", Toast.LENGTH_LONG).show();
 	}
 
 	private boolean isGPSEnabled() {
@@ -384,55 +401,72 @@ public class MenuSportTrack extends Activity implements LocationListener,
 	}
 
 	private boolean isNetworkEnabled() {
-		return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+		return locationManager
+				.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 	}
 
+	// jalan tiap kali gerak/pindah posisi
 	@Override
 	public void onLocationChanged(Location loc) {
 		if (isStart) {
-			
-			LatLng currentLocation = new LatLng(map.getMyLocation().getLatitude(), map.getMyLocation().getLongitude());
+			// Log.d("test","debug "+counter++);
+			/** Algoritma alternatif */
+
+			// Log.d("test","debug "+counter++);
+
+			LatLng currentLocation = new LatLng(map.getMyLocation()
+					.getLatitude(), map.getMyLocation().getLongitude());
 			routePoints.add(currentLocation);
-	
-			Polyline route = map.addPolyline(new PolylineOptions().geodesic(true));
+			Polyline route = map.addPolyline(new PolylineOptions()
+					.geodesic(true));
 			route.setPoints(routePoints);
-			//LatLng currentLatLng = new LatLng(loc.getLatitude(),
-			//		loc.getLatitude());
-			//routePoints.add(currentLatLng);
-			//Polyline route = map.addPolyline(new PolylineOptions()
-			//		.geodesic(true));
-			//route.setPoints(routePoints);
+			// Log.d("test","debug "+counter++);
+
+			/** Algoritma yang seharusnya */
+			/*
+			 * LatLng currentLatLng = new LatLng(loc.getLatitude(),
+			 * loc.getLatitude()); routePoints.add(currentLatLng); Polyline
+			 * route = map.addPolyline(new PolylineOptions() .geodesic(true));
+			 * route.setPoints(routePoints);
+			 */
+			if (startPos == null) {
+				startPos = loc;
+			} else {
+				jarakSampeSekarang += calculateDistance(startPos, loc);
+				startPos = loc;
+
+			}
+			TVAvgSpeed.setText("" + loc.getSpeed());
+
+			// Toast.makeText(getApplicationContext(),"JSS: "+jarakSampeSekarang+" Speed:"+loc.getSpeed(),
+			// Toast.LENGTH_SHORT).show();
+
+			Log.d("test",
+					"berubah" + loc.getLatitude() + ":" + loc.getLongitude());
+
 			
-			Log.d("test", "berubah"+loc.getLatitude()+":"+loc.getLongitude());
-			
-			Toast.makeText(getApplicationContext(),"masuk" + counter++, Toast.LENGTH_SHORT)
-			.show();
-			
+
 			point++;
-			calculateAvgSpeed(loc);
-			
-			
-			
+			avgSpeed = calculateAvgSpeed(loc);
 		}
-		
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
